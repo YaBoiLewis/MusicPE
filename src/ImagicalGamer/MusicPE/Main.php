84515ed00b7f0055f7c54d75b32b0381dc7b6032
Jake <jakehcasto@aol.com>
2016-03-30 21:35:18 -0400

    Create Main.php

---
+++ src/ImagicalGamer/MusicPE/Main.php
@@ -0,0 +1,20 @@
+<?php
+namespace ImagicalGamer\MusicPE;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\math\Vector3;
+use pocketmine\network\protocol\LevelEventPacket;
+use pocketmine\level\sound\GenericSound;
+class Main extends PluginBase implements Listener{
+  public function onEnable(){
+    this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+        if($cmd->getName() == "listen"){
+        $sender->getLevel()->addSound(new PopSound($player));
+
+}
+}
